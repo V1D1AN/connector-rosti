5fe4732832ab23279401c5255ae4d7bd677c0c5d
HADES\V1D1AN <v1d1an.github@gmail.com>
2026-02-23 11:30:11 +0100

    Suppress File

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-OpenCTI MISP Feed Connector (Rosti-compatible)
-
-An external import connector that imports MISP feed data into OpenCTI
-with specific fixes for feeds like Rosti.
-"""
-
-__version__ = "1.0.0"
-__author__ = "V1D1AN"
